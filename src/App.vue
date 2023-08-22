afcd66b3b4646de4a6b7f9213fbc38c04fb05c69
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-08-22 20:34:33 -0300

    Finishing the Exercise

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CalculatorComputed/>
+</template>
+
+<script>
+import CalculatorComputed from './components/CalculatorComputed.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorComputed
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CalculatorComputed.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <input placeholder="num1" type="number" v-model.number="num1">
+    <select v-model="operand"> 
+      <option value="+"> + </option>
+      <option value="-"> - </option>
+      <option value="*"> * </option>
+      <option value="/"> / </option>
+    </select>
+    <input placeholder="num2" type="number" v-model.number="num2">
+    <h2> {{resultadoDaOperacao}} </h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operand: ""
+    };
+  },
+  computed: {
+    resultadoDaOperacao: function() {
+      switch(this.operand) {
+        case "+":
+          return this.num1 + this.num2
+        case "-":
+          return this.num1 - this.num2
+        case "*":
+          return this.num1 * this.num2
+        case "/":
+          return (this.num1 / this.num2).toFixed(2)
+      }
+      return this.resultadoDaOperacao     
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
